97975ed7e38f71084c8e8f851f6df915be554793
Danny Kha <danny.n.kha@gmail.com>
2020-12-06 19:51:27 -0800

    Indexer is good, searcher is halfway

--- src/main/java/edu/uwb/css143b2020fall/service/SearcherImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/SearcherImpl.java
@@ -15,7 +15,22 @@ DO NOT CHANGE
 public class SearcherImpl implements Searcher {
     public List<Integer> search(String keyPhrase, Map<String, List<List<Integer>>> index) {
         List<Integer> result = new ArrayList<>();
-        // add your code
+        List<List<Integer>> var = index.get(keyPhrase);
+        String[] words = keyPhrase.split("\\s+");
+            if (var != null) {
+                for (int i = 0; i < var.size(); i++) {
+                    if (var.get(i).size() > 0) {
+                        result.add(i);
+                    }
+                }
+            }
+            int num = index.get(words[0]).size();
+            for (int i = 0; i < num; i++) {
+                List<List<Integer>> wordList = new ArrayList<>(words.length);
+                for (String word: words) {
+                    wordList.add(index.get(word).get(num));
+                }
+            }
         return result;
     }
 }
